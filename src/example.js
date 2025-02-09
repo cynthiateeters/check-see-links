ee5f3cd27c7cfd473b62b06e5a7adc6da1bb3e28
Cynthia Teeters <cynthiateeters@gmail.com>
2025-02-09 15:28:25 -0500

    Get test to run with example

---
+++ src/example.js
@@ -0,0 +1,13 @@
+/**
+ * A very simple example to demonstrate addition.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Addition
+ */
+
+// Define a function that adds two numbers.
+function add(a, b) {
+  return a + b;
+}
+
+// Use the function and log the result.
+console.log("2 + 3 =", add(2, 3));
